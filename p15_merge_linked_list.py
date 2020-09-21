914aa17829ea49ea7b522cb21d8f8eec05d212b0
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-22 00:10:18 +0530

    Extract NodeList into a reusable datatype

--- p15_merge_linked_list.py
+++ p15_merge_linked_list.py
@@ -9,68 +9,7 @@ list1 = 1->3->5, list2 = 2->4->6->null, return 1->2->3->4->5->6->null
 list1 = 4->4->7, list2 = 1->5->6->null, return 1->4->4->5->6->7->null
 """
 
-from typing import List
-
-
-class NodeList:
-    """Linked List implementation"""
-
-    def __init__(self, value: int, _next=None) -> None:
-        self.value = value
-        self.next = _next
-
-    def print(self) -> None:
-        """Prints the list"""
-        node = self
-        while node is not None:
-            print(node.value, end='->')
-            node = node.next
-        print('null')
-
-    def __add__(self, other):
-        """Merge two linked lists"""
-        node1, node2 = self, other
-        new_list = None
-        node = new_list
-
-        while node1 is not None and node2 is not None:
-            smaller_node = node1 if node1.value < node2.value else node2
-            if not new_list:
-                new_list = smaller_node
-                node = new_list
-            else:
-                node.next = smaller_node
-                node = node.next
-
-            if smaller_node is node1:
-                node1 = node1.next
-            else:
-                node2 = node2.next
-
-        while node1 is not None:
-            node.next = node1
-            node = node.next
-            node1 = node1.next
-
-        while node2 is not None:
-            node.next = node2
-            node = node.next
-            node2 = node2.next
-
-        return new_list
-
-
-def create_node_list(values: List[int]) -> NodeList:
-    """Creates a NodeList out of a list of values"""
-    head = NodeList(values[0])
-
-    last_node = head
-    for value in values[1:]:
-        node = NodeList(value)
-        last_node.next = node
-        last_node = node
-
-    return head
+from data_types.node_list import create_node_list
 
 
 def main():
--- p16_remove_nth_to_last_node.py
+++ p16_remove_nth_to_last_node.py
@@ -11,66 +11,7 @@ Ex: Given the following linked lists...
 
 from typing import List
 
-
-class NodeList:
-    """Linked List implementation"""
-
-    def __init__(self, value: int, _next=None) -> None:
-        self.value = value
-        self.next = _next
-
-    def print(self) -> None:
-        """Prints the list"""
-        node = self
-        while node is not None:
-            print(node.value, end='->')
-            node = node.next
-        print('null')
-
-    def __add__(self, other):
-        """Merge two linked lists"""
-        node1, node2 = self, other
-        new_list = None
-        node = new_list
-
-        while node1 is not None and node2 is not None:
-            smaller_node = node1 if node1.value < node2.value else node2
-            if not new_list:
-                new_list = smaller_node
-                node = new_list
-            else:
-                node.next = smaller_node
-                node = node.next
-
-            if smaller_node is node1:
-                node1 = node1.next
-            else:
-                node2 = node2.next
-
-        while node1 is not None:
-            node.next = node1
-            node = node.next
-            node1 = node1.next
-
-        while node2 is not None:
-            node.next = node2
-            node = node.next
-            node2 = node2.next
-
-        return new_list
-
-
-def create_node_list(values: List[int]) -> NodeList:
-    """Creates a NodeList out of a list of values"""
-    head = NodeList(values[0])
-
-    last_node = head
-    for value in values[1:]:
-        node = NodeList(value)
-        last_node.next = node
-        last_node = node
-
-    return head
+from data_types.node_list import NodeList, create_node_list
 
 
 def remove_nth_to_last_node(head: NodeList, num: int) -> NodeList:
