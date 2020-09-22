9ff56cd9ebbb46bae49d43521ae032341557fdf7
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-22 23:13:22 +0530

    Refactor NodeList to use types

--- data_types/node_list.py
+++ data_types/node_list.py
@@ -1,18 +1,29 @@
 """Standard singly linked list implementation"""
-from typing import List
+from __future__ import annotations
+from typing import List, Optional
 
 
 class NodeList:
     """Linked List implementation"""
 
-    def __init__(self, value: int, _next=None) -> None:
+    def __init__(self, value: int, _next: Optional[NodeList] = None) -> None:
         self.value = value
         self.next = _next
 
-    def __add__(self, other):
+    def __add__(self, other: NodeList) -> NodeList:
         """Merge two linked lists"""
-        node1, node2 = self, other
-        new_list = None
+        node1: Optional[NodeList] = self
+        node2: Optional[NodeList] = other
+        assert node1 is not None and node2 is not None
+
+        smaller_node = node1 if node1.value < node2.value else node2
+        if smaller_node is node1:
+            new_list = node1
+            node1 = node1.next
+        else:
+            new_list = node2
+            node2 = node2.next
+
         node = new_list
 
         while node1 is not None and node2 is not None:
@@ -44,10 +55,10 @@ class NodeList:
     def print(self) -> None:
         """Prints the list"""
         node = self
-        while node is not None:
+        while node.next is not None:
             print(node.value, end='->')
             node = node.next
-        print('null')
+        print(f'{node.value}->null')
 
 
 def create_node_list(values: List[int]) -> NodeList:
