0a242a820cd5b5c0903f0a41388a5afc49d32cc2
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-25 22:46:52 +0530

    Add p23

---
+++ p23_compare_keystrokes.py
@@ -0,0 +1,40 @@
+"""
+Given two strings s and t, which represents a sequence of keystrokes,
+where # denotes a backspace, return whether or not the sequences produce
+the same result.
+
+Ex: Given the following strings...
+
+s = "ABC#", t = "CD##AB", return true
+s = "como#pur#ter", t = "computer", return true
+s = "cof#dim#ng", t = "code", return false
+"""
+from typing import List
+
+
+def apply_backspaces(string: str) -> str:
+    """Process the string with backspace characters into its result"""
+    new_string_chars: List[str] = []
+    for char in string:
+        if char == '#':
+            try:
+                new_string_chars.pop()
+            except IndexError:
+                raise ValueError('Too many backspaces')
+        else:
+            new_string_chars.append(char)
+
+    new_string = ''.join(new_string_chars)
+    return new_string
+
+
+def main():
+    """Main function"""
+    string1 = apply_backspaces(input('> '))
+    string2 = apply_backspaces(input('> '))
+
+    print(string1 == string2)
+
+
+if __name__ == "__main__":
+    main()
