9ce2e6ff744aa081efd1a634bfc42d2007f4748a
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-25 21:55:40 +0530

    Add p21

---
+++ p21_reverse_list.py
@@ -0,0 +1,46 @@
+"""
+Given a linked list, containing unique values, reverse it, and return
+the result.
+
+Ex: Given the following linked lists...
+
+1->2->3->null, return a reference to the node that contains 3 which
+    points to a list that looks like the following: 3->2->1->null
+7->15->9->2->null, return a reference to the node that contains 2 which
+    points to a list that looks like the following: 2->9->15->7->null
+1->null, return a reference to the node that contains 1 which points to
+    a list that looks like the following: 1->null
+"""
+
+from data_types.node_list import NodeList, create_node_list
+
+
+def reverse_list(node_list: NodeList):
+    """Reverse the linked list and return the new head"""
+    head = node_list
+    prev_node, curr_node = head, head.next
+
+    while curr_node is not None:
+        temp = curr_node.next
+        curr_node.next = prev_node
+        prev_node = curr_node
+        curr_node = temp
+
+    last_node = prev_node
+    head.next = None
+
+    return last_node
+
+
+def main():
+    """Main Function"""
+    node_list = create_node_list([1, 2, 3])
+    # node_list = create_node_list([7, 15, 9, 2])
+    # node_list = create_node_list([1])
+
+    node_list = reverse_list(node_list)
+    node_list.print()
+
+
+if __name__ == "__main__":
+    main()
