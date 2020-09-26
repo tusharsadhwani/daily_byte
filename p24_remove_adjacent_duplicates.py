d5a60c094c72397df6d30bbc1ffa42a701642037
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-26 22:52:26 +0530

    Add p24

---
+++ p24_remove_adjacent_duplicates.py
@@ -0,0 +1,34 @@
+"""
+Given a string s containing only lowercase letters, continuously remove
+adjacent characters that are the same and return the result.
+
+Ex: Given the following strings...
+
+s = "abccba", return ""
+s = "foobar", return "fbar"
+s = "abccbefggfe", return "a"
+"""
+from typing import List
+
+
+def remove_adjacent_duplicates(string: str) -> str:
+    """Removes adjacent duplicate characters from string"""
+    stack: List[str] = []
+    for char in string:
+        if len(stack) > 0 and char == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(char)
+
+    new_string = ''.join(stack)
+    return new_string
+
+
+def main():
+    """Main function"""
+    string = input('> ')
+    print(remove_adjacent_duplicates(string))
+
+
+if __name__ == "__main__":
+    main()
