0e82a8ec4e8edddf861c8d7b67607da83b89decc
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-10-11 23:48:50 +0530

    Add p39

---
+++ p39_bottoms_up.py
@@ -0,0 +1,37 @@
+r"""
+Given a binary tree, returns of all its levels in a bottom-up fashion
+(i.e. last level towards the root).
+
+Ex: Given the following tree…
+
+      2
+     / \
+    1   2
+
+return [[1, 2], [2]]
+
+Ex: Given the following tree…
+
+        7
+       / \
+      6   2
+     / \
+    3   3
+
+return [[3, 3], [6, 2], [7]]
+"""
+
+from data_types.node_tree import build_tree, level_order_traversal
+
+
+def main() -> None:
+    """Main function"""
+    tree = build_tree([2, 1, 2])
+    # tree = build_tree([7, [6, 3, 3], 2])
+
+    levels = level_order_traversal(tree)
+    print(levels[::-1])
+
+
+if __name__ == "__main__":
+    main()
