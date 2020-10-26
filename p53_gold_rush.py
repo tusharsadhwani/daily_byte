748d14e914b315367f6168362ab0cfefa44a3206
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-10-26 20:04:58 +0530

    Add prompts for p53 and p54

---
+++ p53_gold_rush.py
@@ -0,0 +1,28 @@
+"""
+Given a 2D matrix that represents a gold mine, where each cell’s value
+represents an amount of gold, return the maximum amount of gold you can
+collect given the following rules:
+
+1. You may start and stop collecting gold from any position
+2. You can never visit a cell that contains 0 gold
+3. You cannot visit the same cell more than once
+
+From current cell, you may walk one cell to the left, right, up, or down
+
+Ex: Given the following gold mine…
+
+goldMine = [
+    [0, 2, 0],
+    [8, 6, 3],
+    [0, 9, 0]
+],
+return 23 (start at 9 and then move to 6 and 8 respectively)
+"""
+
+
+def main() -> None:
+    """Main function"""
+
+
+if __name__ == "__main__":
+    main()
---
+++ p54_generate_parantheses.py
@@ -0,0 +1,24 @@
+"""
+Given an integer N, where N represents the number of pairs of
+parentheses (i.e. ”(“ and ”)”) you are given, return a list containing
+all possible well-formed parentheses you can create.
+
+Ex: Given the following value of N…
+
+N = 3,
+return [
+    "((()))",
+    "(()())",
+    "(())()",
+    "()(())",
+    "()()()"
+]
+"""
+
+
+def main() -> None:
+    """Main function"""
+
+
+if __name__ == "__main__":
+    main()
