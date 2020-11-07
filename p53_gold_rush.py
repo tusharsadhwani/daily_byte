9bd079d54e13ea34872779df21ff8222a2d0f1b8
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-11-07 21:27:27 +0530

    Add p53

--- p53_gold_rush.py
+++ p53_gold_rush.py
@@ -33,7 +33,7 @@ class TrailSet:
     """Holds a set of all cells in a gold trail"""
 
     def __init__(self, trail: Optional[Set[TrailItem]] = None) -> None:
-        self._trail: Set[TrailItem] = trail or set()
+        self._trail: Set[TrailItem] = trail.copy() if trail else set()
 
     def __repr__(self) -> str:
         return f'TrailSet{[item.value for item in self._trail]}'
@@ -49,6 +49,29 @@ class TrailSet:
     def copy(self) -> TrailSet:
         """Creates copy of trail set"""
         new_trail = TrailSet(self._trail)
+        return new_trail
+
+
+class Trail:
+    """Holds an ordered list of all cells in a gold trail"""
+
+    def __init__(self, trail: Optional[List[TrailItem]] = None) -> None:
+        self._trail: List[TrailItem] = trail.copy() if trail else []
+
+    def __repr__(self) -> str:
+        return f'Trail{[item.value for item in self._trail]}'
+
+    def __iter__(self) -> Generator[TrailItem, None, None]:
+        for item in self._trail:
+            yield item
+
+    def add(self, cell: TrailItem) -> None:
+        """Adds cell to trail List"""
+        self._trail.append(cell)
+
+    def copy(self) -> Trail:
+        """Creates copy of trail List"""
+        new_trail = Trail(self._trail)
         return new_trail
 
 
@@ -90,9 +113,92 @@ def flood(
     return trail
 
 
+def find_trail_edges(trail_set: TrailSet, visited: List[List[bool]]) -> Set[TrailItem]:
+    """Finds all the edges in a set of trail items"""
+    # NOTE: I think this breaks when a trail set is of a rectangle shape
+
+    trail_edges: Set[TrailItem] = set()
+    for item in trail_set:
+        row, col = item.row_index, item.col_index
+        rows, cols = len(visited), len(visited[0])
+        neighbour_count = 0
+        for i, j in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+            if row+i < 0 or row+i >= rows or col+j < 0 or col+j >= cols:
+                continue
+
+            if visited[row+i][col+j]:
+                neighbour_count += 1
+
+        if neighbour_count == 1:
+            trail_edges.add(item)
+
+    return trail_edges
+
+
+def find_trails_by_edge(
+        edge: TrailItem,
+        trail_edges: Set[TrailItem],
+        gold_mine: List[List[int]],
+        visited: List[List[bool]],
+        trails: Set[Trail],
+        path: Optional[Trail] = None,
+        visited_neighbours: Optional[Set[TrailItem]] = None) -> None:
+    """Recursive function to find all trails starting from an edge"""
+    if path is None:
+        path = Trail()
+        path.add(edge)
+
+    if visited_neighbours is None:
+        visited_neighbours = set()
+
+    visited_neighbours.add(edge)
+
+    row, col = edge.row_index, edge.col_index
+    rows, cols = len(visited), len(visited[0])
+
+    for i, j in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+        if row+i < 0 or row+i >= rows or col+j < 0 or col+j >= cols:
+            continue
+
+        if not visited[row+i][col+j]:
+            continue
+
+        neighbour = TrailItem(row+i, col+j, gold_mine[row+i][col+j])
+        # TODO: Not sure if this even does anything
+        if neighbour in visited_neighbours:
+            continue
+
+        path_copy = path.copy()
+        path_copy.add(neighbour)
+        if neighbour in trail_edges:
+            trails.add(path_copy)
+        else:
+            visited_neighbours_copy = visited_neighbours.copy()
+            find_trails_by_edge(
+                neighbour,
+                trail_edges,
+                gold_mine,
+                visited,
+                trails,
+                path_copy,
+                visited_neighbours_copy
+            )
+
+
+def find_trails(
+        trail_edges: Set[TrailItem],
+        gold_mine: List[List[int]],
+        visited: List[List[bool]]) -> Set[Trail]:
+    """Finds all full length trails starting from the given edges"""
+    trails: Set[Trail] = set()
+    for edge in trail_edges:
+        find_trails_by_edge(edge, trail_edges, gold_mine, visited, trails)
+
+    return trails
+
+
 def find_max_gold_trail(gold_mine: List[List[int]]) -> int:
     """Finds the gold trail with maximum value and returns its sum"""
-
     visited = [[False for cell in row] for row in gold_mine]
     trail_sets: Set[TrailSet] = set()
 
@@ -104,23 +210,26 @@ def find_max_gold_trail(gold_mine: List[List[int]]) -> int:
 
             trail_sets.add(trail_set)
 
-    for trail_set in trail_sets:
-        # TODO: find edges in trail set
-        #       (a trail is a cell with only 1 valid neighbour)
-        # TODO: for each edge, find all possible trails via the same
-        #       floodfill (floodfill cells which are marked visited, and
-        #       add it to possible trails when you hit an edge)
-        pass
+    max_trail_sum = 0
 
-    return -1
+    for trail_set in trail_sets:
+        trail_edges = find_trail_edges(trail_set, visited)
+        trails = find_trails(trail_edges, gold_mine, visited)
+
+        for trail in trails:
+            trail_sum = sum(item.value for item in trail)
+            if trail_sum > max_trail_sum:
+                max_trail_sum = trail_sum
+
+    return max_trail_sum
 
 
 def main() -> None:
     """Main function"""
     gold_mine = [
-        [0, 2, 0, 0, 0, 2],
-        [8, 6, 3, 0, 0, 2],
-        [0, 9, 0, 0, 0, 2]
+        [0, 2, 0],
+        [8, 6, 3],
+        [0, 9, 0]
     ]
 
     print(find_max_gold_trail(gold_mine))
