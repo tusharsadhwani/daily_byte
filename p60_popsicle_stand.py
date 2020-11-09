990db4e5321dba5c6132bb8a6c0a98dabb7ecee2
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-11-09 23:24:37 +0530

    Add solution to p60

--- p60_popsicle_stand.py
+++ p60_popsicle_stand.py
@@ -1,8 +1,8 @@
 """
 You’re running a popsicle stand where each popsicle costs $5. Each
 customer you encountered pays with either a $5 bill, a $10 bill, or a
-$20 bill and only buys a single popsicle. The customers that come  to
-your stand come in the ordered given by the customers array where
+$20 bill and only buys a single popsicle. The customers that come to
+your stand come in the order given by the customers array where
 customers[i] represents the bill the ith customer pays with. Starting
 with $0, return whether or not you can serve all the given customers
 while also giving the correct amount of change.
@@ -21,10 +21,70 @@ collect $10 from the first customer and we cannot give back change.
 
 Ex: Given the following customers…
 
-customers = [5,5,5,10,20], return true
+customers = [5, 5, 5, 10, 20], return true
 Explanation:
 collect $5 from the first 3 customers.
 collet $10 from the fourth customer and give back $5 change.
 collect $20 from the fifth customer and give back $10 change
 ($10 bill and $5 bill).
 """
+from typing import List
+
+
+def validate_change(customers: List[int]) -> bool:
+    """Validates if the cashier can give change to each customer"""
+    fives, tens, twenties = 0, 0, 0
+
+    def try_pop(notes: int) -> bool:
+        if notes == 0:
+            return False
+
+        notes -= 1
+        return True
+
+    for cash in customers:
+        if cash == 5:
+            fives += 1
+        elif cash == 10:
+            tens += 1
+        elif cash == 20:
+            twenties += 1
+
+        change = cash - 5
+
+        while change > 0:
+            if change >= 20:
+                popped = try_pop(twenties)
+                if not popped:
+                    return False
+                change -= 20
+
+            elif change >= 10:
+                popped = try_pop(tens)
+                if not popped:
+                    return False
+                change -= 10
+
+            elif change >= 5:
+                popped = try_pop(fives)
+                if not popped:
+                    return False
+                change -= 5
+
+            else:
+                return False
+
+    return True
+
+
+def main() -> None:
+    """Main function"""
+    customers = [5, 10]
+    # customers = [10]
+    # customers = [5, 5, 5, 10, 20]
+
+    print(validate_change(customers))
+
+
+if __name__ == "__main__":
+    main()
