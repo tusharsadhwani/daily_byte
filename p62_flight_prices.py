97df19a62c1ef403a47738ede24542988adbba89
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-11-09 23:29:13 +0530

    Fix typos in p62 problem statement

--- p62_flight_prices.py
+++ p62_flight_prices.py
@@ -1,6 +1,6 @@
 """
 A company is booking flights to send its employees to its two satellite
-offices A and B. The cost of sending the I'th employee to office A and 
+offices A and B. The cost of sending the I'th employee to office A and
 office B is given by prices[i][0] and prices[i][1] respectively.
 
 Given that half the employees must be sent to office A and half the
@@ -13,5 +13,5 @@ prices = [[40,30],[300,200],[50,50],[30,60]], return 310
 Fly the first personn to office B.
 Fly the second person to office B.
 Fly the third person to office A.
-Fly the fourth person to office B.
+Fly the fourth person to office A.
 """
