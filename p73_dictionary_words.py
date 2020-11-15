6900f0e3ee22c5f7d43af6c3940cc223e7361335
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-11-15 20:50:44 +0530

    Add questions 73 and 74

---
+++ p73_dictionary_words.py
@@ -0,0 +1,24 @@
+"""
+Given a string s and a list of words representing a dictionary, return
+whether or not the entirety of s can be segmented into dictionary words.
+
+Note: You may assume that all the characters in s and the dictionary are
+lowercase.
+
+Ex: Given the following string s and dictionary...
+s = "thedailybyte", dictionary = ["the", "daily", "byte"], return true.
+
+Ex: Given the following string s and dictionary...
+s = "pizzaplanet", dictionary = ["plane", "pizza"], return false.
+"""
+
+# Using a trie data structure should help here.
+# But just using a trie wont solve all issues, for eg:
+#   s = "planetpizza", dictionary = ["plane", "planet", "pizza"]
+#
+# Using a trie, if we just assume that "plane" is the corrent word and
+# not check ahead for planet, it won't work as "tpizza" is not a word.
+#
+# To make this work, you need to keep checking ahead for the whole
+# string, and check for a possible solution for every word length at
+# which there is a match.
---
+++ p74_longest_increasing_subsequence.py
@@ -0,0 +1,49 @@
+"""
+Given an array of unsorted integers, return the length of its longest
+increasing subsequence.
+
+Note: You may assume the array will only contain positive numbers.
+
+Ex: Given the following array nums...
+
+nums = [1, 9, 7, 4, 7, 13], return 4.
+The longest increasing subsequence is 1, 4, 7, 13.
+"""
+from typing import List
+
+# NOTE: so, i thought this problem is asking for a continuous
+# subsequence, turns out, it wasn't.
+
+
+def find_longest_increasing_subsequence(nums: List[int]) -> List[int]:
+    """Finds the longest continuous increasing subsequence"""
+    max_subsequence_start = 0
+    max_subsequence_length = 0
+
+    start_index = 0
+    for index, num in enumerate(nums):
+        prev_num = nums[index-1]
+        if num < prev_num:
+            # sequence broken
+            subsequence_length = index - start_index
+            if subsequence_length > max_subsequence_length:
+                max_subsequence_start = start_index
+                max_subsequence_length = subsequence_length
+
+            start_index = index
+
+    max_subsequence_end = max_subsequence_start + max_subsequence_length
+    subsequence = nums[max_subsequence_start:max_subsequence_end]
+    return subsequence
+
+
+def main() -> None:
+    """Main function"""
+    nums = [1, 9, 7, 4, 7, 13]
+
+    sequence = find_longest_increasing_subsequence(nums)
+    print(sequence)
+
+
+if __name__ == "__main__":
+    main()
