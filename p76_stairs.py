329d25027299724076cf169f0579fba3a0a961cb
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-11-20 19:28:36 +0530

    Punctuation fix

--- p76_stairs.py
+++ p76_stairs.py
@@ -3,13 +3,13 @@ Given a staircase with N steps and the ability to climb either one or
 two steps at a time, return the total number of ways to arrive at the
 top of the staircase.
 
-Ex: Given the following value of N…
+Ex: Given the following value of N...
 
 N = 2, return 2
 1 step + 1 step
 2 steps
 
-Ex: Given the following value of N…
+Ex: Given the following value of N...
 
 N = 3, return 3
 1 step + 1 step + 1 step
