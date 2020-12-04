f9c5d066510e3428f468e5952f81560e529b370e
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-12-04 21:08:43 +0530

    Add p93

---
+++ p93_defanging_ips.py
@@ -0,0 +1,18 @@
+"""
+Given a valid IP address, defang it.
+Note: To defang an IP address, replace every ”.”, with ”[.]”.
+
+Ex: Given the following address…
+
+address = "127.0.0.1", return "127[.]0[.]0[.]1"
+"""
+
+
+def main() -> None:
+    """Main function"""
+    address = input('> ')
+    print(address.replace('.', '[.]'))
+
+
+if __name__ == "__main__":
+    main()
