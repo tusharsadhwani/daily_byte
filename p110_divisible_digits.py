2377c69b941552e120bfca4b1d9d7b5387e99d78
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-12-23 01:01:00 +0530

    Remove ellipsis

--- p110_divisible_digits.py
+++ p110_divisible_digits.py
@@ -4,7 +4,7 @@ are strictly less than N (starting from one).
 Note: A self divisible number if a number that is divisible by all of
 its digits.
 
-Ex: Given the following value of N…
+Ex: Given the following value of N...
 
 N = 17, return 12 because 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15 are all
 self divisible numbers.
