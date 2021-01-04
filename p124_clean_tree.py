6c091eba0c3899f0400f3d917e90d934628077e7
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-04 20:21:20 +0530

    Add p124

---
+++ p124_clean_tree.py
@@ -0,0 +1,81 @@
+r"""
+This question is asked by Amazon. Given the root of a binary tree where
+every node’s value is either 0 or 1 remove every subtree that does not
+have a 1 in it.
+
+Ex: Given the following binary tree...
+
+      1
+     / \
+    1   0
+
+Return the tree such that it’s been modified to look as follows...
+
+      1
+     /
+    1
+
+Ex: Given the following binary tree...
+
+      1
+     / \
+    1   1
+
+Return the tree such that it’s been modified to look as follows...
+
+      1
+     / \
+    1   1
+
+(No modifications are necessary)
+"""
+from typing import Optional
+from data_types.node_tree import NodeTree, build_tree
+
+
+def is_zeroes(tree: NodeTree) -> bool:
+    """Returns if a subtree is made up of all zeroes"""
+    if tree.value != 0:
+        return False
+
+    if tree.left and not is_zeroes(tree.left):
+        return False
+
+    if tree.right and not is_zeroes(tree.right):
+        return False
+
+    return True
+
+
+def clean_tree(tree: NodeTree) -> Optional[NodeTree]:
+    """Cleans out zero-subtrees in a binary tree"""
+    node: Optional[NodeTree] = tree
+
+    if is_zeroes(node):
+        return None
+
+    if node.left:
+        if is_zeroes(node.left):
+            node.left = None
+    if tree.right:
+        if is_zeroes(node.right):
+            node.right = None
+
+    return node
+
+
+def main() -> None:
+    """Main function"""
+    tree = build_tree([1, 1, 0])
+    # tree = build_tree([1, 1, 1])
+    # tree = build_tree([1, [0, 1, 0], [0, 0, 0]])
+
+    cleaned_tree = clean_tree(tree)
+    if cleaned_tree:
+        cleaned_tree.print_inorder()
+    else:
+        print(None)
+
+
+if __name__ == "__main__":
+    main()
