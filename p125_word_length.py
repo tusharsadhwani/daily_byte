e81b5a995c218bd37d11fde80c71f7e3e2a4a55a
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-09 17:48:35 +0530

    Add p125

---
+++ p125_word_length.py
@@ -0,0 +1,30 @@
+"""
+Given a string, s, return the length of the last word.
+
+Note: You may not use any sort of split() method.
+
+Ex: Given the following string…
+
+s = "The Daily Byte", return 4 (because "Byte" is four characters long).
+"""
+import re
+from string import whitespace as WHITESPACE
+
+
+def main() -> None:
+    """Main function"""
+    string = "The Daily Byte"
+
+    for index, char in enumerate(reversed(string)):
+        if char in WHITESPACE:
+            print(index)
+            break
+    else:
+        print(len(string))
+
+    # Alternative
+    print(len(string) - re.search(r'\b\w+$', string).start())  # type: ignore
+
+
+if __name__ == "__main__":
+    main()
