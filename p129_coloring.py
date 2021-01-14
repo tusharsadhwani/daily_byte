edfff3a4fc4394262b7c627f7c01129c19908063
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-14 22:16:43 +0530

    Add p129

--- p129_coloring.py
+++ p129_coloring.py
@@ -1,4 +1,4 @@
-"""
+r"""
 Given an undirected graph determine whether it is bipartite.
 
 Note: A bipartite graph, also called a bigraph, is a set of graph
@@ -27,29 +27,55 @@ graph = [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]
 
 return false.
 """
+from typing import TypeVar
+
+T = TypeVar('T')
 
 
-from typing import List, Set
+class Group(set[T]):
+    """Named Set"""
+
+    def __init__(self, *, name: str) -> None:
+        super().__init__()
+        self.name = name
+
+    def __repr__(self) -> str:
+        return self.name
 
 
-def is_bipartite(graph: List[List[int]]) -> bool:
-    group_a: Set[int] = set()
-    group_b: Set[int] = set()
+def _is_bipartite(
+        vertex: int,
+        graph: list[list[int]],
+        group: set[int],
+        other_group: set[int]) -> bool:
+    """Recursive definition of is_bipartite"""
+    if vertex in other_group:
+        return False
+
+    if vertex in group:
+        return True
+
+    group.add(vertex)
+    print(vertex, 'added to', group)
+
+    neighbours = graph[vertex]
+    for neighbour in neighbours:
+        if not _is_bipartite(neighbour, graph, group=other_group, other_group=group):
+            return False
+
+    return True
+
+
+def is_bipartite(graph: list[list[int]]) -> bool:
+    """Checks if given graph is bipartite or not"""
+    group_a: set[int] = Group(name='Red')
+    group_b: set[int] = Group(name='Black')
 
     group = group_a
     other_group = group_b
 
-    for vertex, neighbours in enumerate(graph):
-        group.add(vertex)
-        for neighbour in neighbours:
-            if neighbour in group:
-                return False
-
-            other_group.add(neighbour)
-
-        group, other_group = other_group, group
-
-    return True
+    vertex = 0
+    return _is_bipartite(vertex, graph, group, other_group)
 
 
 def main() -> None:
