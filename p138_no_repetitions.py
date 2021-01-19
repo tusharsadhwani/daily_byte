2f34f95aed49916b282f4af296ef44be7da01390
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-19 23:21:27 +0530

    Add p138 problem statement

---
+++ p138_no_repetitions.py
@@ -0,0 +1,22 @@
+"""
+Given a string s, return the length of the longest substring that
+contains only unique characters.
+
+Ex: Given the following string s...
+
+s = "ababbc", return 2.
+
+Ex: Given the following string s...
+
+s = "abcdssa", return 5.
+"""
+
+
+def main() -> None:
+    """Main function"""
+    string = "ababbc"
+    # string = "abcdssa"
+
+
+if __name__ == "__main__":
+    main()
