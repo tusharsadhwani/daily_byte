4838cf61cdf3cb552b76cba3e330dcb74de037e0
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-27 00:10:59 +0530

    Remove ellipsis

--- p143_counting_words.py
+++ p143_counting_words.py
@@ -4,11 +4,11 @@ Given a list of words, return the top k frequently occurring words.
 Note: If two words occur with the same frequency, then the
 alphabetically smaller word should come first.
 
-Ex: Given the following words and value k…
+Ex: Given the following words and value k...
 
 words = ["the", "daily", "byte", "byte"], k = 1, return ["byte"].
 
-Ex: Given the following words and value k…
+Ex: Given the following words and value k...
 
 words = ["coding", "is", "fun", "code", "coding", "fun"], k = 2,
 return ["coding", "fun"].
