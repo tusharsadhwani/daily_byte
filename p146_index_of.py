043b97f9a843704122bb5d1f5e783b7a062070cc
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-27 19:26:21 +0530

    Add own p146 solution

--- p146_index_of.py
+++ p146_index_of.py
@@ -11,11 +11,30 @@ s = "abc", t = "abcd", return -1.
 """
 
 
+def index_of(string: str, substring: str) -> int:
+    """Returns first index of substring in string. If not found, returns -1"""
+    index = 0
+
+    length = len(string)
+    sublength = len(substring)
+
+    while index + sublength <= length:
+        if string[index:index+sublength] == substring:
+            return index
+
+        index += 1
+
+    return -1
+
+
 def main() -> None:
     """Main function"""
     string, substring = "abc", "a"
     # string, substring = "abc", "abcd"
 
+    print(index_of(string, substring))
+
+    # Builtin
     try:
         print(string.index(substring))
     except ValueError:
